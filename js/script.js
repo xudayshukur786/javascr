69401e201ca5bb92beee4db0ea0a470f6047b206
xudayshukur786 <146706253+xudayshukur786@users.noreply.github.com>
2023-10-02 19:48:07 +0500

    commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+// ** ** ** ** ** ** ** * 1 - lesson ** ** ** ** **
+let number = 10;
+const borderColor = "red";
+
+const person = {
+    neme: "xudoyshukur",
+};
+
+person.name = "Abdumuminov";
+console.log(person)
+
+"use strict"
+number = 10
+console.log(number);
+
+//number
+let ummber = 5.6; // Data Type = Number
+console.log(-8 / 0); // Infinity
+console.log("xudoyshukur" * 8); // NaN
+
+//string
+const clientName = 'xudoyshukur';
+
+//boolean
+const isMarried = false;
+const isCircleEarth = true;
+
+
+//null
+// console.log(clientAge);
+
+
+//undefined
+let und;
+console.log(und);
+
+
+//object
+const thief = {
+    //key: value
+    age: 30, //number
+    jacket: "black", //string
+    isLong: false, //boolean
+};
+console.log(thief.jacket);
+console.log(thief.age);
+console.log(thief);
+
+
+// array
+//                0       1        2      3   4   5      
+const colors = ["red", "black", "gre  en", 10, [], {}];
+
+console.log(colors[0]);
+console.log(colors[4]);
